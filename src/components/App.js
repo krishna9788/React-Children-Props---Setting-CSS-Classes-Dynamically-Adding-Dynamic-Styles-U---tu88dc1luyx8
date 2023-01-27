76068023dfdaf03404ffe79d0458c09adb02f3fa
krishna9788 <krishnakushwaha978@gmail.com>
2023-01-27 22:42:19 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,21 @@ import '../styles/App.css';
 import ChildComponent from './ChildComponent';
 const App = () => {
  //code here
+  const [square, setSquare] = useState(0);
+
+  const squareNumber = (e) => {
+    let number = e.target.value;
+    // console.log(number, typeof (number))
+    let s = number * number;
+    setSquare(s);
+  }
 
   return (
     <div id="main">
       <ChildComponent /> 
-       <input id='input' onChange={} />
+       <input id='input' onChange={squareNumber} />
     
-      <p id='output'>{}</p>
+      <p id='output'>{square}</p>
     </div>
   )
 }
